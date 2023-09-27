9aa4c0eb9ca3a8a8e9dd5bf8cfe0522645bc5bb8
andrearuggiero150 <a.ruggiero150@studenti.unisa.it>
2023-09-27 12:48:39 +0200

    prova 2

--- src/test/java/CalcolatriceTest.java
+++ src/test/java/CalcolatriceTest.java
@@ -5,20 +5,37 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalcolatriceTest {
+
     Calcolatrice calcolatrice;
 
     @Before
-    public final void setUp() {
-        calcolatrice = new Calcolatrice();
+
+    public final void setUp(){
+        calcolatrice=new Calcolatrice();
     }
 
     @Test
-    public void TestSomma() {
-        assertTrue(calcolatrice.somma(11, 1) == 12, "somma corretta");
+    public void testSomma(){
+        assertTrue(calcolatrice.somma(11,1)==12, "somma corretta");
     }
 
     @Test
-    public void TestMoltiplicazione() {
-        assertTrue(calcolatrice.moltiplicazione(10, 2) == 20, "moltiplicazione corretta");
+    public void testMoltiplicazione(){
+        assertTrue(calcolatrice.moltiplicazione(10,2)==20, "moltiplicazione corretta");
+    }
+
+    @Test
+    public void testSottrazione(){
+        assertTrue(calcolatrice.sottrazione(5,3)==2, "sottrazione corretta");
+    }
+
+    @Test
+    public void testDivisione(){
+        assertTrue(calcolatrice.divisione(10,2)==5, "divisione corretta");
+    }
+
+    @Test
+    public void testDivisionePerZero(){
+        assertTrue(calcolatrice.divisione(10,0)==0, "divisione corretta");
     }
 }
